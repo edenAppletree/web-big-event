0203f12e11f000dbc8320060ef34572f7a8407d8
limengting <1443848358@qq.com>
2022-06-11 09:46:30 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,24 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
+// 请求拦截器 可以在这个函数中，对请求参数进行处理
 $.ajaxPrefilter((options) => {
     // 在请求之前拼接上根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
+    // 注入token
+    if(options.url.includes('/my/')) {
+        options.headers={
+            Authorization: localStorage.getItem("token")
+        }
+    }
+    // 权限校验 身份认证
+    options.complete = (res) => {
+        if (res.responseJSON.status === 1 &&
+            res.responseJSON.message === '身份认证失败！') {
+            // 强制清空token
+            localStorage.removeItem('token')
+            // 跳转到登录页面
+            location.href = '/home/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+// 获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        type: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem("token")
+        // },
+        success: (res) => {
+            // console.log(res);
+            if (res.status !== 0) return layer.msg(res.message)
+            layer.msg('获取用户信息成功')
+            // 调用渲染函数
+            renderAvatar(res.data)
+        },
+        // complete: (res) => {
+        //     console.log(res);
+        //     if (res.responseJSON.status === 1 &&
+        //         res.responseJSON.message === '身份认证失败！') {
+        //         // 强制清空token
+        //         localStorage.removeItem('token')
+        //         // 跳转到登录页面
+        //         location.href = '/home/login.html'
+        //     }
+        // }
+    })
+}
+// 渲染用户信息
+const renderAvatar = (user) => {
+    const name = user.nickname || user.username
+    // 渲染欢迎语
+    $('#welcome').html(`欢迎 ${name}`)
+    // 按需渲染头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        const first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
+
+// 退出登录
+$('#btnLogout').click(() => {
+    layer.confirm('是否退出登录？', { icon: 3, title: '提示' }, function () {
+        // 1.清除本地存储的token
+        localStorage.removeItem('token')
+        // 2.跳转到登录页面
+        location.href = '/home/login.html'
+    })
+})
+
+// 获取用户列表 调用函数
+getUserInfo()
